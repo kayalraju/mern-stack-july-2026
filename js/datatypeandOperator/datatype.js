3ca1314a47e3d237d74f3aa05ef58c82ef7bf307
LearningDeVops-png <kayalraju004@gmail.com>
2026-07-20 10:15:17 +0530

    first commit

---
+++ js/datatypeandOperator/datatype.js
@@ -0,0 +1,25 @@
+
+const user={
+    name:"shubham", // string
+    age:22, // number
+    isAdmin:true // boolean
+}
+
+
+//console.log('user age is',user.age);
+//console.log('typeof user',typeof user);
+
+
+
+//array
+const numbers=[1,2,3,4,5];
+//console.log('numbers',numbers);
+//console.log(numbers[4])
+
+const styudent= ["raju","pritam","subir","joya"];
+//console.log('student',styudent);
+//console.log(styudent[2]);
+
+
+// let age = null;
+// console.log(age)
---
+++ js/datatypeandOperator/operator.js
@@ -0,0 +1,17 @@
+
+
+// const number=20
+// const number2=10
+// const result=number+number2
+// console.log(result);
+
+
+//syntax of ternary operator
+
+ 
+
+
+const age = 10;
+const status = age >= 18 ? "Adult" : "Minor";
+console.log(status);
+
