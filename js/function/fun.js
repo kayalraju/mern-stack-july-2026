43f4dee8252fc6195e6f63954298d011a1e412b0
LearningDeVops-png <kayalraju004@gmail.com>
2026-07-30 20:52:51 +0530

    array

--- js/function/fun.js
+++ js/function/fun.js
@@ -95,3 +95,102 @@
 // fruits.forEach(function(fruit) {
 //     console.log(fruit);
 // });
+
+
+//************************ aray metrhods */
+
+//*****map */
+// array.map(callback)
+
+// const users = [
+//     { name: "Rahul", age: 20 },
+//     { name: "Priya", age: 22 },
+//     { name: "John", age: 25 },
+// ];
+
+// const names = users.map(user => user.age);
+
+// console.log(names);
+
+//filter()
+//Returns elements that satisfy a condition.
+// const products = [
+//     { name: "Laptop", price: 60000 },
+//     { name: "Mouse", price: 500 },
+//     { name: "Keyboard", price: 1000 },
+//     { name: "Monitor", price: 15000 },
+// ];
+
+// const expensive = products.filter(product => product.price > 1000);
+
+// console.log(expensive);
+
+//find()
+
+//Returns the first matching element..
+// const products = [
+//     { name: "Laptop", price: 60000 },
+//     { name: "Mouse", price: 500 }
+// ];
+
+// const expensive = products.find(product => product.name === "Laptop");
+
+// console.log(expensive);
+
+//reduce()
+//converts an array into a single value.
+//syntax
+// array.reduce((accumulator, currentValue) => {
+//     // logic
+// }, initialValue);
+
+// const products = [
+//     { name: "Laptop", price: 60000 },
+//     { name: "Mouse", price: 500 }
+// ];
+
+// const total = products.reduce((total, product) => total + product.price, 0);
+
+// console.log(total);
+
+//push()
+//syntax
+// array.push(item1, item2, ...)
+
+// const fruits = ["Apple", "Banana", "Orange"];
+
+// fruits.push("Mango");
+
+// console.log(fruits);
+
+
+//pop
+
+// const fruits = ["Apple", "Banana", "Orange"];
+
+// fruits.pop("Orange");
+
+// console.log(fruits);
+
+//syntax
+// array.splice(startIndex, deleteCount, item1, item2, ...)
+// startIndex → Position where you want to insert.
+// deleteCount → Number of elements to remove (0 means don't remove any).
+// item1, item2... → Elements to insert.
+
+// const fruits = ["Apple", "Banana", "Orange"];
+
+// fruits.splice(1, 1, "Mango");
+
+// console.log(fruits);
+
+
+//shift
+//syntax
+// array.shift();
+
+ const fruits = ["Apple", "Banana", "Orange"];
+
+ fruits.shift("Apple");
+
+ console.log(fruits);
