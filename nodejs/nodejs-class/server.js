ff549953078a6c569b61d1b4f0bf9ba96cb874a8
RajuKayal <learnbackend228@gmail.com>
2026-09-08 09:40:58 +0530

    ejs templete engine

--- nodejs/nodejs-class/server.js
+++ nodejs/nodejs-class/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express=require('express');
 const ejs=require('ejs');
+const path=require('path');
 
 
 
@@ -8,6 +9,10 @@ const app=express();
 
 app.set('view engine','ejs');
 app.set('views','./src/views');
+//create statioc folder
+app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 const homeRoute=require('./src/router/home.route');
 app.use(homeRoute);
